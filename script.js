52f6b6a52cb86562bd862b6d7b0b6b55c3827a36
Pawel Kremienowski <Kremienowski33@gmail.com>
2024-01-24 20:16:30 +0100

    setup

---
+++ script.js
@@ -0,0 +1,8 @@
+console.log("hello world");
+let num = 10
+console.log(num);
+console.log(typeof (num));
+
+const companyName = document.getElementById("companyName")
+companyName.innerHTML = "New company name"
+companyName.style.backgroundColor = "#ff0000"
